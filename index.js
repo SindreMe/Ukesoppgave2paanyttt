6b34540cc5be6e84a60e265191efd932d1e41ab2
SindreMe <122259979+SindreMe@users.noreply.github.com>
2023-02-06 12:47:59 +0100

    Random images

---
+++ index.js
@@ -0,0 +1,9 @@
+//Lag en funksjon som setter kilden til det bildet du trykker på,
+//og en funksjon som returnerer navnet på et av de 3 bildene.
+
+function randomImage(element) {
+
+    element.innerHTML = 'heihei'
+}
+
+
